f419dca75cea4bcd18f301704f4cd79cf5cdce9c
latenuu <latenuu@gmail.com>
2024-06-01 23:11:38 +0300

    Version 2

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -10,7 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.print("Tekstiä ");
-        System.out.printfln("tänne.");
+        System.out.println("tänne.");
         System.out.println(666);
         System.out.println(667); //tulostetaaan luku
         System.out.println(668);
